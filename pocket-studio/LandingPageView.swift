b7c69ee27a33984cb48408c6749381f63f10b3ba
Daniel <102876965+Dauniel@users.noreply.github.com>
2023-06-04 16:12:43 -0700

    adding new changes to the landing page

--- pocket-studio/LandingPageView.swift
+++ pocket-studio/LandingPageView.swift
@@ -1,24 +1,22 @@
-//just story title 1
-//talk to UI/UX about the landing page
-//click the story would give an overview of the chapters
-//settings page instead of profile view
 import SwiftUI
 
 struct Story {
     let title: String
-    let author: String
+    let authors: [String]
     let rating: Double
     let duration: String
     let numberOfStickers: Int
+    let coverImageName: String
+    let artists: [String]
 }
 
 struct LandingPageView: View {
     @State private var username: String
     @State private var stories: [Story] = [
-        Story(title: "Story Title 1", author: "Author 1", rating: 4.5, duration: "30min", numberOfStickers: 10),
-        Story(title: "Locked", author: "Author 2", rating: 4.7, duration: "20min", numberOfStickers: 5),
-        Story(title: "Locked", author: "Author 3", rating: 4.2, duration: "45min", numberOfStickers: 8)
+        Story(title: "Hopping\nthrough\nthe Seasons", authors: ["Jocelyn Mendoza"], rating: 4.5, duration: "30min", numberOfStickers: 10, coverImageName: "book_cover", artists: ["Jennie Gao", "Ronald Lu", "Joyce Pang"]),
+        Story(title: "Locked", authors: ["Author 2"], rating: 4.7, duration: "20min", numberOfStickers: 5, coverImageName: "book_cover", artists: ["Artist 3"])
     ]
+    @State private var selectedStoryIndex = 0
 
     init(username: String = "_____") {
         _username = State(initialValue: username)
@@ -27,66 +25,134 @@ struct LandingPageView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("Recommended Stories")
-                    .font(.custom("Inter Bold", size: 24))
+                Spacer()
+                Text("Stories").font(.custom("Aleo Regular", size: 40))
+                    .bold()
+                    .foregroundColor(Color(#colorLiteral(red: 1, green: 0.88, blue: 0.62, alpha: 1)))
+                    .tracking(4)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(LinearGradient(gradient: Gradient(colors: [Color(.sRGB, red: 0.13, green: 0.3, blue: 0.21), Color(.sRGB, red: 0.26, green: 0.33, blue: 0.21)]), startPoint: .leading, endPoint: .trailing))
+                            .frame(width: 220, height: 70) // Adjust the width and height as desired
+                            .shadow(color: .black, radius: 20, x: 0, y: 2) // Add shadow with desired properties
 
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHStack(spacing: 16) {
-                        ForEach(stories, id: \.title) { story in
-                            VStack(alignment: .leading) {
-                                NavigationLink(destination: destinationView(for: story)) {
-                                    RoundedRectangle(cornerRadius: 25)
-                                        .fill(Color(#colorLiteral(red: 0.5541666746139526, green: 0.5541666746139526, blue: 0.5541666746139526, alpha: 1)))
-                                        .frame(width: 248, height: 379)
-                                }.buttonStyle(PlainButtonStyle())
+                    )
+                
+                TabView(selection: $selectedStoryIndex) {
+                    ForEach(stories.indices, id: \.self) { index in
+                        NavigationLink(destination: destinationView(for: stories[index])
+                            .navigationBarBackButtonHidden(true)) {
+                            ZStack(alignment: .topLeading) {
+                                Image(stories[index].coverImageName)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .cornerRadius(20)
+                                    .frame(width: 650, height: 650)
+                                    .shadow(color: .black, radius: 10, x: 2, y: 2) // Add shadow with desired properties
+                                VStack(alignment: .leading, spacing: 3) {
+                                    Text("\n")
+                                    Text(stories[index].title)
+                                        .font(.system(size: 24, weight: .bold))
+                                        .bold()
+                                        .foregroundColor(Color(#colorLiteral(red: 0.14, green: 0.17, blue: 0.21, alpha: 1)))
+                                        .tracking(2.4)
+                                        .padding(.leading, 190)
+                                        .fixedSize(horizontal: false, vertical: true)
+                                    
+                                    Text("Art by")
+                                        .foregroundColor(Color(#colorLiteral(red: 0.14, green: 0.17, blue: 0.21, alpha: 1)))
+                                        .tracking(2.4)
+                                        .padding(.leading, 190)
+                                        .fixedSize(horizontal: false, vertical: true)
 
-                                VStack(alignment: .leading, spacing: 4) {
-                                    Text(story.title)
-                                        .font(.headline)
-                                    Text("Author: \(story.author)")
-                                        .font(.subheadline)
-                                    Text("Rating: \(story.rating)")
-                                        .font(.subheadline)
-                                    Text("Duration: \(story.duration)")
-                                        .font(.subheadline)
-                                    Text("Stickers: \(story.numberOfStickers)")
-                                        .font(.subheadline)
+                                    Text(stories[index].artists.joined(separator: "\n"))
+                                        .font(.system(size: 18, weight: .bold))
+                                        .foregroundColor(Color(#colorLiteral(red: 0.14, green: 0.17, blue: 0.21, alpha: 1)))
+                                        .tracking(2.4)
+                                        .padding(.leading, 190)
+                                        .fixedSize(horizontal: false, vertical: true)
+
+                                    Text("Written by")
+                                        .foregroundColor(Color(#colorLiteral(red: 0.14, green: 0.17, blue: 0.21, alpha: 1)))
+                                        .tracking(2.4)
+                                        .padding(.leading, 190)
+                                        .fixedSize(horizontal: false, vertical: true)
+
+                                    Text(stories[index].authors.joined(separator: ", "))
+                                        .font(.system(size: 18, weight: .bold))
+                                        .foregroundColor(Color(#colorLiteral(red: 0.14, green: 0.17, blue: 0.21, alpha: 1)))
+                                        .tracking(2.4)
+                                        .padding(.leading, 190)
+                                        .fixedSize(horizontal: false, vertical: true)
                                 }
+                                .padding(.top, 10)
+
                             }
                         }
+                        .buttonStyle(PlainButtonStyle())
+                        .tag(index)
                     }
-                    .padding()
                 }
-
+                .tabViewStyle(PageTabViewStyle())
+                .padding()
+                
                 HStack {
-                    NavigationLink(destination: JournalView()) {
-                        Image(systemName: "book")
+                    Spacer()
+
+                    NavigationLink(destination: LandingPageView()
+                        .navigationBarBackButtonHidden(true)) {
+                        Image(systemName: "book.fill")
+                            .font(.system(size: 30)) // Adjust the size to make the icon bigger
                             .padding()
-                            .background(Color.black)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
+                            .foregroundColor(Color(#colorLiteral(red: 1, green: 0.88, blue: 0.62, alpha: 1)))
+                            .cornerRadius(10)
                     }
-                    NavigationLink(destination: UserProfileView(username: username)) {
-                        Image(systemName: "person")
+                    Spacer()
+
+                    NavigationLink(destination: JournalView()
+                        .navigationBarBackButtonHidden(true)) {
+                        Image(systemName: "square.and.pencil")
+                            .font(.system(size: 30)) // Adjust the size to make the icon bigger
                             .padding()
-                            .background(Color.black)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
+                            .foregroundColor(Color(#colorLiteral(red: 1, green: 0.88, blue: 0.62, alpha: 1)))
+                            .cornerRadius(10)
                     }
+                    Spacer()
+
+                    NavigationLink(destination: settingsView()
+                        .navigationBarBackButtonHidden(true)) {
+                        Image(systemName: "gearshape")
+                            .font(.system(size: 30)) // Adjust the size to make the icon bigger
+                            .padding()
+                            .foregroundColor(Color(#colorLiteral(red: 1, green: 0.88, blue: 0.62, alpha: 1)))
+                            .cornerRadius(10)
+                    }
+                    Spacer()
                 }
-                .padding(.top)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color(.sRGB, red: 0.13, green: 0.3, blue: 0.21), Color(.sRGB, red: 0.26, green: 0.33, blue: 0.21)]), startPoint: .leading, endPoint: .trailing))
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: 500, height:100) // Adjust the width and height as desired
+
+                )
             }
-            .navigationTitle("Landing Page")
+            .background(
+                Image("Landing_Page")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+            )
         }
     }
 
     private func destinationView(for story: Story) -> some View {
         switch story.title {
-        case "Story Title 1":
+        case "Hopping\nthrough\nthe Seasons":
+            return AnyView(story_1_chapters())
+        case "Locked":
             return AnyView(story_1_panel_1())
-        case "Story Title 2":
-            return AnyView(story_1_panel_1())
-        // add more cases as needed
         default:
             return AnyView(StoryView(story: story))
         }
@@ -106,6 +172,3 @@ struct StoryView: View {
         Text("Story: \(story.title)")
     }
 }
-
-
-// remember to add the views for other stories here
