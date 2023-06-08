8dfba9b320bea07366f4b5b7b3205cfa80c97cf1
Daniel <102876965+Dauniel@users.noreply.github.com>
2023-06-07 22:59:54 -0700

    adding/updating panels

---
+++ story_1_panel_7.swift
@@ -0,0 +1,70 @@
+import SwiftUI
+
+struct story_1_panel_7 : View {
+    @State private var switchView: Bool = false
+    @State private var chaptersView: Bool = false
+
+    private let layers = [
+        Layer(id: 0, imageName: "bunnyriver2", depthEffect: 1.0),
+    ]
+
+    var body: some View {
+        ZStack {
+            if chaptersView {
+                story_1_chapters()
+            } else {
+                Group {
+                    if switchView {
+                        AnyView(story_1_panel_8())
+                    } else {
+                        AnyView(
+                            GeometryReader { geometry in
+                                ZStack() {
+                                    ForEach(layers, id: \.id) { layer in
+                                        Image(layer.imageName)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                                    }
+                                }
+                            }
+                            .edgesIgnoringSafeArea(.all)
+                        )
+                    }
+                }
+                VStack {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            self.chaptersView = true
+                        }) {
+                            Image(systemName: "ellipsis")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.black)
+                                .padding()
+                        }
+                    }
+                    Spacer()
+                    Button(action: {
+                        self.switchView = true
+                    }) {
+                        Text("Next")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(40)
+                    }
+                    .padding([.leading, .trailing], 20)
+                }
+            }
+        }
+    }
+}
+
+struct story_1_panel_7_Previews: PreviewProvider {
+    static var previews: some View {
+        story_1_panel_7()
+    }
+}
